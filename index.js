d5f9686567680cff5f0e4c4c51dff3e818f1f248
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2016-10-29 20:44:06 +0300

    create a server connection using express

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 
-var http = require('http');
+var express = require('express'),
+    morgan = require('morgan');
 
 var hostname = 'localhost';
 var port = 8080;
 
 // create new server
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.write('Test');
-  response.end();
-})
+var app = express();
 
-server.listen(port, hostname, function () {
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function () {
   console.log('Server running at http://' + hostname + ':' + port + '/');
 });
