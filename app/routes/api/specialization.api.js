43cd56a95960f57a3643b1c700d5b7681d4e6e17
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2016-12-10 21:33:55 +0200

    added method for select,insert,update and delete in specialization API

--- app/routes/api/specialization.api.js
+++ app/routes/api/specialization.api.js
@@ -18,27 +18,50 @@ export class Api {
       res.send('spec api');
     })
 
-    this.router.get('/add', function (req, res) {
-      res.send('add specializationS');
-
+    this.router.post('/add', function (req, res) {
+      var reqBody = req.body;
+      if (!reqBody)
+        return res.status(500).send({ 'message': 'error' });
       var spec = {
-        name: "romana",
-        short_name: "RO"
+        name: reqBody.name,
+        short_name: reqBody.short_name
       }
 
       Specialization.add(spec);
+      res.send({ 'message': 'success' })
     })
 
-    this.router.get('/showAll', function (req, res) {
+    this.router.get('/find', function (req, res) {
       Specialization.findAll().then(function (results) {//return all specialization when promise returned something
         res.send(results)
       })
     })
 
-    this.router.get('/show/:id', function (req, res) {
+    this.router.get('/find/:id', function (req, res) {
       Specialization.findById(req.params.id).then(function (result) {
         res.send(result)
       })
+    })
+
+    this.router.put('/:id', function (req, res) {
+      var reqBody = req.body;
+      var spec = {
+        specialization_id: req.params.id,
+        name: reqBody.name,
+        short_name: reqBody.short_name
+      }
+      Specialization.update(spec)
+      res.send({ 'message': 'success' })
+    })
+
+    this.router.delete('/:id', function (req, res) {
+      Specialization.delete(req.params.id).then(function (affectedRows) {
+        if (!affectedRows)
+          res.status(500).send({ 'message': 'error' })
+        else
+          res.send({ 'message': 'success' })
+      })
+
     })
 
   }
