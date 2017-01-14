02a28b37df7fc12a29aa996a811cd7835410050e
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-01-15 00:23:13 +0200

    added auth middleware into api routes

--- app/routes/group.api.js
+++ app/routes/group.api.js
@@ -15,45 +15,55 @@ export class Api {
         //create new Router
         this.router = express.Router()
 
-        this.router.post('/add', (req, res) => {
-            Handler.post(req.body, (error, result) => {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.post('/add',
+            this.middlewares.auth.webAuth,
+            (req, res) => {
+                Handler.post(req.body, (error, result) => {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.get('/find', (req, res) => {
-            Handler.getAll((error, result) => {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.get('/find',
+            this.middlewares.auth.webAuth,
+            (req, res) => {
+                Handler.getAll((error, result) => {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.get('/find/:id', (req, res) => {
-            Handler.getOne(req.params.id, (error, result) => {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.get('/find/:id',
+            this.middlewares.auth.webAuth,
+            (req, res) => {
+                Handler.getOne(req.params.id, (error, result) => {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.put('/:id', (req, res) => {
-            Handler.put(req.body, req.params.id, (error, result) => {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.put('/:id',
+            this.middlewares.auth.webAuth,
+            (req, res) => {
+                Handler.put(req.body, req.params.id, (error, result) => {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.delete('/:id', (req, res) => {
-            Handler.delete(req.params.id, (error, result) => {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.delete('/:id',
+            this.middlewares.auth.webAuth,
+            (req, res) => {
+                Handler.delete(req.params.id, (error, result) => {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
     }
 }
--- app/routes/note.api.js
+++ app/routes/note.api.js
@@ -16,45 +16,55 @@ export class Api {
         //create new Router
         this.router = express.Router()
 
-        this.router.post('/add', function (req, res) {
-            Handler.post(req.body, function (error, result) {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.post('/add',
+            this.middlewares.auth.webAuth,
+            function (req, res) {
+                Handler.post(req.body, function (error, result) {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.get('/find', function (req, res) {
-            Handler.getAll(function (error, result) {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.get('/find',
+            this.middlewares.auth.webAuth,
+            function (req, res) {
+                Handler.getAll(function (error, result) {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.get('/find/:id', function (req, res) {
-            Handler.getOne(req.params.id, function (error, result) {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.get('/find/:id',
+            this.middlewares.auth.webAuth,
+            function (req, res) {
+                Handler.getOne(req.params.id, function (error, result) {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.put('/:id', function (req, res) {
-            Handler.put(req.body, req.params.id, function (error, result) {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.put('/:id',
+            this.middlewares.auth.webAuth,
+            function (req, res) {
+                Handler.put(req.body, req.params.id, function (error, result) {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
-        this.router.delete('/:id', function (req, res) {
-            Handler.delete(req.params.id, function (error, result) {
-                if (error)
-                    return res.status(500).send(error)
-                return res.status(200).send(result)
+        this.router.delete('/:id',
+            this.middlewares.auth.webAuth,
+            function (req, res) {
+                Handler.delete(req.params.id, function (error, result) {
+                    if (error)
+                        return res.status(500).send(error)
+                    return res.status(200).send(result)
+                })
             })
-        })
 
     }
 }
