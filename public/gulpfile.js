dc0803ccbcfc0c8092e698f488e7cff0697683fb
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-02-20 20:56:31 +0200

    added sass compiler to css minifed an unminified in gulp file

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    cleanCSS = require('gulp-clean-css'),
+    rename = require('gulp-rename');
 
 var src = 'app/assets';
 
 gulp.task('sass', function () {
+    // css unminified
     gulp.src(src + '/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest(src + '/css/'))
-        //todo: implement minified css
+        .pipe(sass({errLogToConsole: true}))
+        .pipe(gulp.dest(src + '/css/'));
+    //css minified
+    gulp.src(src + '/sass/**/*.scss')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest(src + '/css/'));
 });
 
+//todo: add minification task for js
+
 //Watch task
-gulp.task('default', function () {
-    gulp.watch(src + 'sass/**/*.scss', ['styles']);
+gulp.task('watch', function () {
+    gulp.watch(src + '/sass/**/*.scss', ['sass']);
 });
