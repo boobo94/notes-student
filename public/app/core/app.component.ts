a9d919d9bc81725c494364425109e0ecdd77da02
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-02-22 18:01:08 +0200

    added angular core component, module and main file

---
+++ public/app/core/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'notes-students',
+    template: `
+        <h1>Notes Students</h1>
+    `
+})
+export class AppComponent { }
---
+++ public/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './core/app.module';
+
+// run in browser
+platformBrowserDynamic().bootstrapModule(AppModule);
