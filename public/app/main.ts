a1fa84aa43c96304a23a64cb24e374eccd267164
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-02-25 13:45:54 +0200

    added logger to main ts

--- public/app/main.ts
+++ public/app/main.ts
@@ -2,4 +2,10 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './core/app.module';
 
 // run in browser
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .then(function (success) {
+        return console.log('Loading with sucess!')
+    })
+    .catch(function (error) {
+        return console.log(error);
+    })
