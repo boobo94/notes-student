96e8712b71fefdb271f8ada9208d7e2c2a04b0bb
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-02-26 04:11:52 +0200

    added home component

---
+++ public/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'home',
+    template: `
+            <h1>This is the homepage</h1>
+    `
+})
+export class HomeComponent {
+
+}
