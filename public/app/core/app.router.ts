7a732fa07689426a6c07add448db60aecf98048f
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-02-26 04:12:17 +0200

    added base router

---
+++ public/app/core/app.router.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from '../components/home/home.component';
+
+const appRoutes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    }
+];
+
+export const baseRoute = RouterModule.forRoot(appRoutes);
