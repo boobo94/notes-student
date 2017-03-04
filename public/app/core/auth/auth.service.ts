f7b51e5f9741cc37e0b6f4690c9998f2d80bad46
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-03-04 16:48:25 +0200

    check if user is logged in

--- public/app/core/auth/auth.service.ts
+++ public/app/core/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { tokenNotExpired } from 'angular2-jwt';
 import 'rxjs/add/operator/toPromise';
 
 import { ApiUrls } from '../urls.config';
@@ -7,7 +8,7 @@ import { ApiUrls } from '../urls.config';
 @Injectable()
 export class AuthService {
     constructor(private http: Http, private urls: ApiUrls) {
-        
+
     }
 
     login(user: any): Promise<any> {
@@ -19,6 +20,10 @@ export class AuthService {
             .catch((error) => {
                 console.log(error);
             })
+    }
+
+    loggedIn() {
+        return tokenNotExpired('userToken');//userToken is saved in localStorage with this name
     }
 
     //todo: singup
