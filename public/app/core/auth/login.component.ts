b70110e7dae5cd1e71e58aa249cefdc54dcb7c6a
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-03-04 19:07:52 +0200

    login template customized

--- public/app/core/auth/login.component.ts
+++ public/app/core/auth/login.component.ts
@@ -7,17 +7,19 @@ import { AuthService } from './auth.service';
     selector: 'login',
     template: `
                 <div class="row">
-                    <div class="input-field col s12">
+                    <div class="input-field col s10 offset-s1 m6 offset-m3 l4 offset-l4">
                         <input id="username" type="text" class="validate" [(ngModel)]="user.username">
                         <label for="username">Username</label>
                     </div>
-                    <div class="input-field col s12">
+                    <div class="input-field col s10 offset-s1 m6 offset-m3 l4 offset-l4">
                         <input id="password" type="password" class="validate" [(ngModel)]="user.password">
                         <label for="password">Password</label>
                     </div>
-                    <button class="btn waves-effect waves-light right" type="submit" (click)="login()">Submit
-                        <i class="material-icons right">send</i>
-                    </button>
+                    <div class="col s10 offset-s1 m6 offset-m3 l4 offset-l4">
+                        <button class="btn waves-effect waves-light right" type="submit" (click)="login()">Submit
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </div>
                 </div>
     `
 })
