ab4f3d763db233dacb03c3837fdc0edcab9dd378
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-03-06 22:12:54 +0200

    config provideAuth with headerName and so on for jwt

--- public/app/core/auth/auth.module.ts
+++ public/app/core/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule} from '@angular/forms';
+import { provideAuth } from 'angular2-jwt';
 
 import { LoginComponent } from './login.component';
 import { SignUpComponent } from './signup.component';
@@ -28,7 +29,15 @@ import { Messages } from '../messages.config';
         AuthService,
         AuthGuard,
         ToastService,
-        Messages
+        Messages,
+        provideAuth({
+            headerName: 'X-Auth-Token',//name of my custom header used in api
+            headerPrefix: 'Bearer',
+            tokenName: 'userToken',
+            tokenGetter: (() => localStorage.getItem('userToken')),
+            globalHeaders: [{ 'Content-Type': 'application/json' }],
+            //noJwtError: true
+        })
     ]
 })
 export class AuthModule {
