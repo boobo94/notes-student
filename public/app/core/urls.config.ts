c5740da9d20d24d25f21537de679f2a8676a664a
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-03-06 21:34:32 +0200

    added urls

--- public/app/core/urls.config.ts
+++ public/app/core/urls.config.ts
@@ -4,11 +4,20 @@ import { Injectable } from '@angular/core';
 export class ApiUrls {
     baseApiUrl = 'http://localhost:8080/'
 
+    //auth
+
     loginUrl = this.baseApiUrl + 'api/auth/login' //POST
     signUpUrl = this.baseApiUrl + 'api/auth/signup' //GET
-
-    getStudentByRegistrationNumber = this.baseApiUrl + 'api/student/findbyrn/' //GET and /:registration_number
+    
+    //users
 
     getStudentByRN = this.baseApiUrl + 'api/user/findbyrn/' //GET and /:registration_number
     getStudentByUN = this.baseApiUrl + 'api/user/findbyun/' //GET and /:username
+
+    //students
+
+    getStudentByRegistrationNumber = this.baseApiUrl + 'api/student/findbyrn/' //GET and /:registration_number
+
+    //specializations
+    getAllSpecializations = this.baseApiUrl + 'api/specialization/find'
 }
