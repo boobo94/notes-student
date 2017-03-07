e8053448a4f22dca2c4741da7e7af96104282d2e
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-03-07 10:28:05 +0200

    get all specialization in component

--- public/app/components/specializations/specializations.component.ts
+++ public/app/components/specializations/specializations.component.ts
@@ -6,17 +6,28 @@ import { SpecializationsService } from './specializations.service';
 @Component({
     selector: 'specialization-component',
     template: `
-    
+    s
     `
 })
 export class SpecializationsComponent implements OnInit {
     specializations: any[]
 
-    constructor( private service: SpecializationsService, private router: Router) {
+    constructor(private service: SpecializationsService, private router: Router) {
 
     }
 
     ngOnInit(): void {
+
+    }
+
+    getAllSpecializations(): void {
+        this.service.getAllSpecializations()
+            .then((r) => {
+                this.specializations = r.data;
+            })
+            .catch((error) => {
+                console.log(error)
+            })
 
     }
 }
