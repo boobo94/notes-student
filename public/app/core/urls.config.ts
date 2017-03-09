e651bcba69bf48c19dd4e058a996295a7bf2cbcb
Bogdan Alexandru Militaru <bmilitaru@airtouchmedia.com>
2017-03-09 18:11:14 +0200

    added api urls for students

--- public/app/core/urls.config.ts
+++ public/app/core/urls.config.ts
@@ -14,14 +14,18 @@ export class ApiUrls {
     getStudentByRN = this.baseApiUrl + 'api/user/findbyrn/' //GET and /:registration_number
     getStudentByUN = this.baseApiUrl + 'api/user/findbyun/' //GET and /:username
 
-    //students
-
-    getStudentByRegistrationNumber = this.baseApiUrl + 'api/student/findbyrn/' //GET and /:registration_number
-
     //specializations
-    getAllSpecializations = this.baseApiUrl + 'api/specialization/find' //GET
-    getSpecialization = this.baseApiUrl + 'api/specialization/find/' //GET and /:specialization_id
-    updateSpecialization = this.baseApiUrl + 'api/specialization/' //PUT and /:specialization_id
-    deleteSpecialization = this.baseApiUrl + 'api/specialization/' //DELETE and /:specialization_id
-    insertSpecialization = this.baseApiUrl + 'api/specialization/' //POST and /:specialization_id
+    getAllSpecializations   = this.baseApiUrl + 'api/specialization/find' //GET
+    getSpecialization       = this.baseApiUrl + 'api/specialization/find/' //GET and /:specialization_id
+    updateSpecialization    = this.baseApiUrl + 'api/specialization/' //PUT and /:specialization_id
+    deleteSpecialization    = this.baseApiUrl + 'api/specialization/' //DELETE and /:specialization_id
+    insertSpecialization    = this.baseApiUrl + 'api/specialization/' //POST and /:specialization_id
+
+    //students
+    getStudentByRegistrationNumber = this.baseApiUrl + 'api/student/findbyrn/' //GET and /:registration_number
+    getAllStudents          = this.baseApiUrl + 'api/student/find' //GET
+    getStudents             = this.baseApiUrl + 'api/student/find/' //GET and /:sstudent_id
+    updateStudents          = this.baseApiUrl + 'api/student/' //PUT and /:student_id
+    deleteStudents          = this.baseApiUrl + 'api/student/' //DELETE and /:student_id
+    insertStudents          = this.baseApiUrl + 'api/student/' //POST and /:student_id
 }
