dc8196be29ad59d4ec87b463f315f2ca6168a73a
boobo94 <boobo94@Macs-MacBook-Pro.local>
2017-07-18 17:05:58 +0300

    prepare to run in prod

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,6 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './core/app.module';
-
+import { enableProdMode } from '@angular/core';
+enableProdMode();
 // run in browser
 platformBrowserDynamic().bootstrapModule(AppModule)
     .then(function (success) {
